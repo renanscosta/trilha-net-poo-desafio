ad3c65b25f215ffa03fdb12d604045456a223ccc
Renan Costa <renan.costa.ti@gmail.com>
2025-06-10 13:13:45 -0300

    adição das propriedades faltantes na classe Smartphone; Implementação do método InstalarAplicativo; Implementação das heranças necessarias

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+var nokia = new Nokia("11999999999", "Nokia 6.1", "1234567890", 64);
+var iphone = new Iphone("11988888888", "iPhone 12", "0987654321", 128);
+nokia.Ligar();
+iphone.Ligar();
+nokia.ReceberLigacao();
+iphone.ReceberLigacao();
+nokia.InstalarAplicativo("WhatsApp");
+iphone.InstalarAplicativo("Telegram");
+Console.WriteLine("Aplicativos instalados com sucesso!");
+Console.WriteLine("Pressione qualquer tecla para sair...");
+Console.ReadKey();
